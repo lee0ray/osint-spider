49a54113a614b8b921b7c4270ef6811c85afad91
XiaoyuZHK <haikun9804@163.com>
2023-10-09 10:36:18 +0800

    delete next-url

--- osint/osint/spiders/fortinet.py
+++ osint/osint/spiders/fortinet.py
@@ -12,14 +12,14 @@ class FortinetSpider(scrapy.Spider):
     pages = 22
 
     def start_requests(self):
+        for i in range(0, self.pages):
+            self.start_urls.append(self.next_urls[0] + str(i) + self.next_urls[1])
 
         for i in self.start_urls:
             # scrapy.Request()
             yield scrapy.Request(url=i, callback=self.parse, dont_filter=True)
-        for i in range(0, self.pages):
-            yield scrapy.Request(url = self.next_urls[0] + str(i) + self.next_urls[1],
-                                 callback = self.parsenext, dont_filter = True)
 
+            
     def parse(self, response):
         self.logger.info(response)
         for post in response.xpath('//div[contains(@class, "b3-blog-list__post text-container")]'):
@@ -41,29 +41,6 @@ class FortinetSpider(scrapy.Spider):
             item['extract']['publish_time'] = publish_time
             item['extract']['authors'] = authors
             
-            yield SeleniumRequest(url=source_url, wait_time=5, callback=self.parse_content, meta={'item': item})
-
-    def parsenext(self, response):
-        self.logger.info(response)
-        for post in response.xpath('//div[contains(@class, "b3-blog-list__post text-container")]'):
-            _source_url = post.xpath('div[1]/div[2]/h2/a/@href').extract_first()
-            source_url = response.urljoin(_source_url)
-            title = post.xpath('div[1]/div[2]/h2/a/text()').extract_first()
-            abstract = post.xpath('div[1]/div[2]/p[2]/text()').extract_first()
-            tags = []
-            source = self.name
-            publish_time = post.xpath('div[2]/div[2]/p/span[2]/text()').extract_first()
-            authors = [i.strip() for i in post.xpath('div[2]/div[2]/p/span[1]/a/text()').extract()]
-            item = OsintItem()
-            item['title'] = title
-            item['abstract'] = abstract
-            item['tags'] = tags
-            item['source'] = source
-            item['source_url'] = source_url
-            item['extract'] = {}
-            item['extract']['publish_time'] = publish_time
-            item['extract']['authors'] = authors
-
             yield SeleniumRequest(url=source_url, wait_time=5, callback=self.parse_content, meta={'item': item})
 
     def parse_content(self,  response: scrapy.http.Response):
